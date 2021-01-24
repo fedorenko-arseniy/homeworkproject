53533c9db26231ec82a536948a7de719403016d5
fedorenko-arseniy <77456310+fedorenko-arseniy@users.noreply.github.com>
2021-01-24 16:54:29 +0300

    Update math.py

--- math.py
+++ math.py
@@ -1,1 +1,25 @@
-int(input(Введите число : ))
+numbers = []
+does = []
+i = 0
+while i < 2 :
+    number = int(input('Введите число : '))
+    numbers.append(number)
+    do = input('Введите знак : ')
+    does.append(do)
+    i = i + 1
+
+
+
+if does[0] == '+':
+    answer = numbers[0] + numbers[1]
+    print(f'{numbers[0]} + {numbers[1]} = {answer}')
+elif does[0] == '-':
+    answer = numbers[0] - numbers[1]
+    print(f'{numbers[0]} - {numbers[1]} = {answer}')
+elif does[0] == '*':
+    answer = numbers[0] * numbers[1]
+    print(f'{numbers[0]} * {numbers[1]} = {answer}')
+elif does[0] == '/':
+    answer = numbers[0] / numbers[1]
+    print(f'{numbers[0]} / {numbers[1]} = {answer}
+
